224033863c55766975f9230d7dabb400dbd160a6
Stefan Daschek <stefan@daschek.net>
2014-12-29 22:12:29 +0100

    Replace magic values with constants.

--- lib/display.rb
+++ lib/display.rb
@@ -1,7 +1,12 @@
 require "socket"
 
 class Display
-  EMPTY_LINE = " " * 144
+  PANEL_WIDTH = 48
+  NUM_PANELS  = 3
+  TOTAL_WIDTH = PANEL_WIDTH * NUM_PANELS
+  HEIGHT      = 120
+  
+  EMPTY_LINE = " " * TOTAL_WIDTH
   
   attr_reader :ips, :port, :socket
   
@@ -14,25 +19,25 @@ class Display
   def show(frame)
     frame = pad_frame(frame)
     bytes = [[], [], []]
-    0.upto(2).each do |panel|
-      0.upto(47).each do |col|
+    NUM_PANELS.times do |panel|
+      PANEL_WIDTH.times do |col|
         byte = ""
-        119.downto(0).each do |row|
-          byte += frame[row][panel * 48 + col] != " " ? "1" : "0"
+        (HEIGHT - 1).downto(0).each do |row|
+          byte += frame[row][panel * PANEL_WIDTH + col] != " " ? "1" : "0"
           if byte.length == 8
             bytes[panel] << byte.to_i(2)
             byte = ""
           end
         end
       end
-      socket.send bytes[panel].pack("c" * 720), 0, ips[panel], port
+      socket.send bytes[panel].pack("c" * (PANEL_WIDTH * HEIGHT / 8)), 0, ips[panel], port
     end
   end
   
   # Pad frame with blank lines and spaces so we get exactly 120 lines, containing 144 characters each.
   def pad_frame(frame)
-    frame = frame.map{ |line| line.center(144) }    
-    blank_lines = 120 - frame.size
+    frame = frame.map{ |line| line.center(TOTAL_WIDTH) }    
+    blank_lines = HEIGHT - frame.size
     [EMPTY_LINE] * (blank_lines / 2.0).ceil + frame + [EMPTY_LINE] * (blank_lines / 2.0).floor
   end
 end
